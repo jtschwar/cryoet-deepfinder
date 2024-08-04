f61e54dcddc3603b97fd9c3c871f2b3f592c4d48
jtschwar <jtschwar@gmail.com>
2024-08-04 16:48:38 -0700

    Flexible entry point

--- step1.py
+++ step1.py
@@ -72,7 +72,7 @@ def parse_seg_target(ctx, param, value):
 @click.option("--out-name", type=str, default="spheretargets", help="Target name.")
 @click.option("--out-user-id", type=str, default="train-deepfinder", help="User ID for output.")
 @click.option("--out-session-id", type=str, default="0", help="Session ID for output.")
-def create_local(
+def create(
     config: str,
     target: List[Tuple[str, Union[str, None], Union[str, None], int]],
     seg_target: List[Tuple[str, Union[str, None], Union[str, None]]],
@@ -127,87 +127,6 @@ def process_create_command(
     # Load CoPick root
     copickRoot = copick.from_file(config)
 
-    process_create_command(config, train_targets, tomo_ids, voxel_size, tomogram_algorithm, out_name, out_user_id, out_session_id, is_dataportal=False)
-
-
-@cli.command()
-@click.option("--config", type=str, required=True, help="Path to the configuration file.")
-@click.option(
-    "--target",
-    type=(str, int),
-    required=False,
-    help="Tuples of object name and radius (in voxels).",
-    multiple=True,
-)
-@click.option(
-    "--seg-target",
-    type=(str),
-    required=False,
-    help="Object name for segmentation.",
-    multiple=True,
-)
-@click.option(
-    "--tomo-ids",
-    type=str,
-    required=False,
-    default=None,
-    show_default=True,
-    help="Comma separated list of Tomogram IDs.",
-)
-@click.option("--voxel-size", type=float, default=10, help="Voxel size.")
-@click.option("--tomogram-algorithm", type=str, default="wbp", help="Tomogram algorithm.")
-@click.option("--out-name", type=str, default="spheretargets", help="Target name.")
-@click.option("--out-user-id", type=str, default="train-deepfinder", help="User ID for output.")
-@click.option("--out-session-id", type=str, default="0", help="Session ID for output.")
-def create_from_dataportal(
-    config: str,
-    target: List[Tuple[str, int]],
-    seg_target: List[ str ],
-    tomo_ids: str,
-    voxel_size: float = 10,
-    tomogram_algorithm: str = "wbp",
-    out_name: str = "spheretargets",
-    out_user_id: str = "train-deepfinder",
-    out_session_id: str = "0",
-):
-    # Load CoPick root
-    copickRoot = copick.from_file(config)
-
-    train_targets = {}
-    for t in target:
-        info = {
-            "label": copickRoot.get_object(t[0]).label,
-            "radius": t[1],
-            "is_particle_target": True,
-        }
-        train_targets[t[0]] = info
-
-    for t in seg_target:
-        info = {
-            "label": copickRoot.get_object(t).label,
-            "radius": None,
-            "is_particle_target": False,
-        }
-        train_targets[t] = info
-
-    # Additional logic to source data from the data portal can be added here
-    process_create_command(config, train_targets, tomo_ids, voxel_size, tomogram_algorithm, out_name, out_user_id, out_session_id, is_dataportal=True)
-
-
-def process_create_command(
-    config: str,
-    train_targets: dict,
-    tomo_ids: str,
-    voxel_size: float,
-    tomogram_algorithm: str,
-    out_name: str,
-    out_user_id: str,
-    out_session_id: str,
-    is_dataportal: bool,
-):
-    # Load CoPick root
-    copickRoot = copick.from_file(config)
-
     target_names = list(train_targets.keys())
 
     # Radius list
@@ -259,16 +178,11 @@ def process_create_command(
         query = []
         for target_name in target_names:
             if train_targets[target_name]["is_particle_target"]:
-                if is_dataportal:
-                    query += copickRun.get_picks(
-                        object_name=target_name
-                    )
-                else:
-                    query += copickRun.get_picks(
-                        object_name=target_name,
-                        user_id=train_targets[target_name]["user_id"],
-                        session_id=train_targets[target_name]["session_id"],
-                    )
+                query += copickRun.get_picks(
+                    object_name=target_name,
+                    user_id=train_targets[target_name]["user_id"],
+                    session_id=train_targets[target_name]["session_id"],
+                )
 
         # Read Particle Coordinates and Write as Segmentation
         objl_coords = []
